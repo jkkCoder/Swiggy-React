1f8676439cd273e1db438d9dee470d49c8b75ae0
jkkCoder <jayeshk118@gmail.com>
2023-08-26 23:54:57 +0530

    chapter 2 igniting our app

--- App.js
+++ App.js
@@ -1,9 +1,12 @@
+import React from "react"
+import ReactDOM  from "react-dom/client"
+
 const parent = React.createElement(
     'div',
     {id:"parent"},
     [
         React.createElement( 'div', {id:'child'}, [
-            React.createElement('h1',{},"I'm an h1 tag"),
+            React.createElement('h1',{},"I'm an Namaste React course  "),
             React.createElement('h2',{},"I'm an h2 tag")
         ]),
         React.createElement('div',{id:'child2'},[
