f1acb2ff21daf3973cff84cf9abca21bb71be384
jayeshmagilhub <jayesh.kumar@magilhub.com>
2023-09-05 23:57:59 +0530

    chapter 6 exploring the world

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,12 +1,40 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { restaurantList } from "../utils/mocks";
 import RestaurantCard from "./RestaurantCard";
+import Shimmer from "./Shimmer";
 
 const Body = () => {
-    const [listOfRestaurants , setListOfRestaurants] = useState(restaurantList)
-    return (
+    const [listOfRestaurants , setListOfRestaurants] = useState([])
+    const [filteredRestaurants, setFilteredRestaurants] = useState([])
+
+    const [searchText, setSearch] = useState("")
+
+    useEffect(() => {
+        fetchData();
+    },[])
+
+    const fetchData = async () => {
+        const data = await fetch('https://www.swiggy.com/dapi/restaurants/list/v5?lat=21.1702401&lng=72.83106070000001&page_type=DESKTOP_WEB_LISTING')
+        const json = data.json()
+
+        console.log(json) 
+        // setListOfRestaurants(json?.data?.card[2]?.data?.data?.cards);     //api has changed, this is fetching wrong so uisng mock data only
+        setListOfRestaurants(restaurantList)
+        setFilteredRestaurants(restaurantList)
+    }
+
+    return filteredRestaurants  .length === 0 ?
+        <Shimmer />
+    : (
         <div className="body">
             <div className="filter">
+                <div className="search">
+                    <input type="text" className="searc-box" value={searchText} onChange={e => setSearch(e.target.value)}/>
+                    <button onClick={() => {
+                        const filteredRestaurant = listOfRestaurants.filter(res => res.data.name.toLowerCase().includes(searchText.toLowerCase()))
+                        setFilteredRestaurants(filteredRestaurant)
+                    }}>search</button>
+                </div>
                 <button className="filter-btn" onClick={() => {
                     const filteredData = restaurantList?.filter(list => parseFloat(list.data.avgRating) > 4)
                     setListOfRestaurants(filteredData)
@@ -14,11 +42,11 @@ const Body = () => {
             </div>
             <div className="res-container">
                 {
-                    listOfRestaurants?.map(obj => <RestaurantCard  key={obj.id} resObj={obj}/>)
-                }
+                    filteredRestaurants?.map(obj => <RestaurantCard  key={obj.id} resObj={obj}/>)
+                } 
             </div>
         </div>
-    )
+    )         
 }
 
 export default Body;
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,10 @@
+import { useState } from "react";
 import { LOGO_URL } from "../utils/constants";
 
 const Header = () => {
+
+    const [btnName,setBtnName] = useState("Login")
+
     return (
         <div className="header">
             <div className="logo-container">
@@ -12,6 +16,7 @@ const Header = () => {
                     <li>About us</li>
                     <li>Contact us</li>
                     <li>Cart</li>
+                    <button onClick={() => {btnName === 'Login' ? setBtnName("Logout") : setBtnName('Login')}} className="login">{btnName}</button>
                 </ul>
             </div>
         </div>
